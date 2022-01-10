31c3c14f358451a4fff971d1e1b9fc08d23cfe5b
jmamama <96282326+jmamama@users.noreply.github.com>
2022-01-10 14:21:36 +0900

    나의 앱 만들어보기(생활코딩)
    나의 앱 만들어보기(생활코딩)

---
+++ MyApp/AccountingIFApp.java
@@ -0,0 +1,39 @@
+
+public class AccountingIFApp {
+
+	public static void main(String[] args) {
+	
+		double valueOFSupply = Double.parseDouble(args[0]);
+		double vatRate = 0.1;
+		double expenseRate = 0.3;
+		double vat = valueOFSupply*vatRate;
+		double total = valueOFSupply + vat;
+		double expense = valueOFSupply*expenseRate;
+		double income = valueOFSupply-expense;
+		
+		double dividend1; 
+		double dividend2; 
+		double dividend3; 
+		
+		if(income > 10000.0) {
+			 dividend1 = income * 0.5;
+			 dividend2 = income * 0.3;
+			 dividend3 = income * 0.2;
+		} else {
+			 dividend1 = income * 1.0;
+			 dividend2 = income * 0;
+			 dividend3 = income * 0;
+		}
+		System.out.println("Value of supply : "+ valueOFSupply);
+		System.out.println("VAT : "+ vat);
+		System.out.println("Total : "+ total);
+		System.out.println("Expense : "+ expense);
+		System.out.println("Income : "+ income);
+		System.out.println("Dividend1 : "+ dividend1);
+		System.out.println("Dividend2 : "+ dividend2);
+		System.out.println("Dividend3 : "+ dividend3);  
+
+
+	}
+
+}
